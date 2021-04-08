9e310e7d26a17aed0ffcfbd62660febaad5f11be
kavitaneha <64353021+kavitaneha@users.noreply.github.com>
2021-04-09 01:26:30 +0530

    Add files via upload

---
+++ Presenter.java
@@ -0,0 +1,30 @@
+package com.karumi.superheroesproject.ui.presenter;
+
+public class Presenter<T extends Presenter.View> {
+
+  private T view;
+
+  public void setView(T view) {
+    this.view = view;
+  }
+
+  public T getView() {
+    return view;
+  }
+
+  public void initialize() {
+
+  }
+
+  public void update() {
+
+  }
+
+  public interface View {
+
+    void showLoading();
+
+    void hideLoading();
+
+  }
+}
